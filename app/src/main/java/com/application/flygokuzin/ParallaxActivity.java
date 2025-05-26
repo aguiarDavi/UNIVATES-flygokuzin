801fee090db12ac79872bc15bd8107ee98bed233
Rafael Eduardo Dreissig <rafaeldreissig@gmail.com>
2025-05-26 20:18:39 -0300

    classe ParallaxActivity atualizada para adicionar um efeito loop na imagem de fundo.

--- app/src/main/java/com/application/flygokuzin/ParallaxActivity.java
+++ app/src/main/java/com/application/flygokuzin/ParallaxActivity.java
@@ -6,26 +6,36 @@ import android.graphics.Canvas;
 public class ParallaxActivity {
 
     private Bitmap image;
-    private float y;
     private float speed;
-    private int alturaTela;
+    private int screenHeight;
 
-    public ParallaxActivity(Bitmap bitmap, float speed, int alturaTela){
-        this.image = bitmap;
+    private float y1, y2;
+
+    public ParallaxActivity(Bitmap image, float speed, int screenHeight) {
+        this.image = Bitmap.createScaledBitmap(image, image.getWidth(), screenHeight, true);
         this.speed = speed;
-        this.y = 0;
-        this.alturaTela = alturaTela;
+        this.screenHeight = screenHeight;
+
+        y1 = 0;
+        y2 = -screenHeight; // Começa fora da tela para cima
     }
 
-    public void update(){
-        y += speed;
-        if(y >= alturaTela){
-            y = 0;
+    public void update() {
+        y1 += speed;
+        y2 += speed;
+
+        // Reinicia posições para loop
+        if (y1 >= screenHeight) {
+            y1 = y2 - screenHeight;
+        }
+
+        if (y2 >= screenHeight) {
+            y2 = y1 - screenHeight;
         }
     }
 
-    public void draw(Canvas canvas){
-        canvas.drawBitmap(image, 0, y, null);
-        canvas.drawBitmap(image, 0, y - alturaTela, null);
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, 0, y1, null);
+        canvas.drawBitmap(image, 0, y2, null);
     }
 }
