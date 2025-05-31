acaeb2ba15de764ef141c037ea537cd659a753bf
mathevargas <matheusvargask2@gmail.com>
2025-05-31 02:08:28 -0300

    Melhorias na tela inicial com layout ajustado, novo botão de voltar ao início na tela de Game Over, e correções na exibição do recorde salvo ao retornar à tela inicial.

--- app/src/main/java/com/application/flygokuzin/GameOverActivity.java
+++ app/src/main/java/com/application/flygokuzin/GameOverActivity.java
@@ -1,6 +1,7 @@
 package com.application.flygokuzin;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,10 +19,26 @@ public class GameOverActivity extends AppCompatActivity {
 
         TextView scoreText = findViewById(R.id.textScoreFinal);
         Button restartButton = findViewById(R.id.btnRestart);
+        Button btnTelaInicial = findViewById(R.id.btnTelaInicial);
 
         scoreText.setText("Score Final: " + score);
+
+        // SALVAR RECORD SE NECESSÁRIO
+        SharedPreferences prefs = getSharedPreferences("game_data", MODE_PRIVATE);
+        int recordeSalvo = prefs.getInt("recorde", 0);
+        if (score > recordeSalvo) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("recorde", score);
+            editor.apply();
+        }
+
         restartButton.setOnClickListener(v -> {
             startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
+
+        btnTelaInicial.setOnClickListener(v -> {
+            startActivity(new Intent(this, StartActivity.class));
             finish();
         });
     }
