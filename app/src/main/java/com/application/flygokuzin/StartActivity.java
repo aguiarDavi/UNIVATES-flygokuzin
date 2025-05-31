7850fe71a47a03e69da1af7ad6e8cdd64891f271
aguiarDavi <daviim0510@gmail.com>
2025-05-31 11:59:18 -0300

    Implement Start Menu song

--- app/src/main/java/com/application/flygokuzin/StartActivity.java
+++ app/src/main/java/com/application/flygokuzin/StartActivity.java
@@ -2,6 +2,7 @@ package com.application.flygokuzin;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class StartActivity extends AppCompatActivity {
 
     private TextView recordeScore;
+    private MediaPlayer startMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,11 @@ public class StartActivity extends AppCompatActivity {
 
         Button btnIniciar = findViewById(R.id.btnIniciar);
         btnIniciar.setOnClickListener(v -> {
+            if (startMusic != null) {
+                startMusic.stop();
+                startMusic.release();
+                startMusic = null;
+            }
             Intent intent = new Intent(StartActivity.this, MainActivity.class);
             startActivity(intent);
         });
@@ -34,6 +41,31 @@ public class StartActivity extends AppCompatActivity {
         int recorde = prefs.getInt("recorde", 0);
         if (recordeScore != null) {
             recordeScore.setText("Record Score: " + recorde);
+        }
+        if (startMusic == null) {
+            startMusic = MediaPlayer.create(this, R.raw.start);
+            startMusic.setLooping(true);
+            startMusic.start();
+        } else if (!startMusic.isPlaying()) {
+            startMusic.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (startMusic != null && startMusic.isPlaying()) {
+            startMusic.pause();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (startMusic != null) {
+            startMusic.stop();
+            startMusic.release();
+            startMusic = null;
         }
     }
 }
